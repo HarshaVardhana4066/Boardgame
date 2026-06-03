b9b2e7f94c6ca772996b6fa5edc8f77f904b6cab
Harsha Vardhana M <ha40127553@wipro.com>
2026-06-03 08:45:11 +0530

    added code to sg

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,12 @@
+resource "aws_instance" "roboshop" {
+    ami = "ami-073130f74f5ffb161"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = aws_security_group.by_terraform.id
+}
+
+
+
+
 resource "aws_security_group" "by_terraform" {
 
     name = "allowall"
